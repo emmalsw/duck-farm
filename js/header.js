682ad594c9fbda4e7a2689741e64444d241cfd54
Emma <emmaswleung@gmail.com>
2019-08-31 09:51:13 +0800

    add header plugin and carousel duck

---
+++ js/header.js
@@ -0,0 +1,39 @@
+(function ( $ ) {
+ 
+    $.fn.loadHeader = function() {
+        this.html(
+      `<div class="container-fluid">
+        <nav class="navbar navbar-light" style="background-color: #ffff99">
+          <a class="navbar-brand" href="#">
+            <img src="/img/ducky_logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+            Dally the Duck Duck Farm
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#">Meet Some Ducks</a>
+              </li>
+              <li>
+                  <a class="nav-link" href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>`);
+        return this;
+    };
+ 
+}( jQuery ));
+
+
+
