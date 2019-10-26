316bb3765e1381ed7d818818dc813a3138e6bc88
Emma <emmaswleung@gmail.com>
2019-10-26 09:58:59 +0800

    added for loop

--- js/shopItemInfo.js
+++ js/shopItemInfo.js
@@ -1,21 +1,23 @@
 (function ( $ ) {
  
-    $.fn.loadshopItemInfo = function(picture,words,quantity_of_ducks_left) {
-        this.html(
+    $.fn.loadshopItemInfo = function(array) {
+        var content = "";
+        for (var obj of array) {
+            content = content +
             `<div class="row" style= "padding-bottom: 10px">
                 <div class="col-sm-2">
-                    <img class="shop_ducks" src="${picture}" style="width: 250px;height: 250px">
+                    <img class="shop_ducks" src="${obj.pic}" style="width: 100%;height: 100%">
                 </div>
                 <div class="col-sm">
-                    <p>${words}</p>
+                    <p>${obj.words}</p>
                 </div>
                 <div class="col-sm-3">
-                    <button type="button" class="btn btn-primary">Buy this duck</button>
-                    <p>Ducks Left: ${quantity_of_ducks_left}</p>
+                    <a class="btn btn-primary" target="_blank" href="https://forms.gle/Mz5AH2WuxovuYpC76">${obj.btnName}</a>
+                    <p>Ducks Left: ${obj.quantity}</p>
                 </div>
-            </div>
-             `
-        );
+            </div>`
+        }
+        this.html(content);
         return this;
     };
  
